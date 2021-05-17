7a8e984122309ca6becc4aa9960d8320ab3288d3
the_mcmurder <the_mcmurder@users.noreply.github.com>
2021-05-17 13:53:38 -0600

    showing the issue

--- src/root.jsx
+++ src/root.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import {Provider, defaultTheme, Button} from '@adobe/react-spectrum';
 
 export default function RootComponent() {
   return (
-    <div>Hello from react</div>
+    <Provider theme={defaultTheme}>
+      <div>Hello from react</div>
+      <Button variant="cta" onPress={() => alert('Hey there!')}>
+        Hello React Spectrum!
+      </Button>
+    </Provider>
   )
+  gg
 }
