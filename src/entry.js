1e13743cadfb32c63b1384d9203139a37cd6cfe9
the_mcmurder <the_mcmurder@users.noreply.github.com>
2021-05-18 11:54:01 -0600

    trying the beta release

--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-console.log('hey')
 import Root from './root.jsx'
 
 const domNode = document.querySelector('main')
-console.log('domNode', domNode)
 ReactDOM.render(<Root />, domNode)
